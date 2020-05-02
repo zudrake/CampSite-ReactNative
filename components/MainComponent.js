a65c22dc07ea0f89063ffa975c8cb0bdfc2e85fb
Drake <zudrake.yh@gmail.com>
2020-05-01 21:13:21 -0700

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -13,6 +13,9 @@ import {
     fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners
 } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
+
+
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -66,6 +69,28 @@ const ContactNavigator = createStackNavigator(
         })
     }
 )
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: {
@@ -155,6 +180,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({ tintColor }) => (
                     <Icon
                         name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({ tintColor }) => (
+                    <Icon
+                        name='tree'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
